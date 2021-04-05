6ff8a387d278030f1c725deb1437dc06145e0eba
piotrsupport991 <piotrnowicki38383@gmail.com>
2021-04-05 11:44:17 -0500

    docker volume bookmarking

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,21 @@ import express from "express";
 import path from "path";
 //import cookieParser from "cookie-parser";
 import logger from "morgan";
-import cors from 'cors'
-import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+const Pool = require("pg").Pool;
+
+//import { graphqlHTTP } from "express-graphql";
 //import schema from './graphql/schema/schema'
 //const indexRouter = require("./routes/index");
 //const usersRouter = require("./routes/users");
 
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "docker_practice",
+  password: "password",
+  port: 5432,
+});
 
 let app = express();
 
@@ -16,20 +25,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 
-
-app.use(cors())
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send("test")
-})
+app.get("/", (req, res) => {
+  const users = pool
+    .query(`SELECT * FROM users`)
+    .then((data: any) => {
+        return data.rows 
+    }).catch((err: any) => {
+        throw err
+    })
+    res.send(users)
+});
 
 // app.use('/graphql', graphqlHTTP({
-//     schema, 
+//     schema,
 //     graphiql: !(process.env.NODE_ENV == 'production')
 // }))
-
 
 // app.use("/", indexRouter);
 //app.use("/users", usersRouter);
